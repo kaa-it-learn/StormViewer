b65459881d593dc1df0743e00cede3c506b9522b
Admin <admin@mac-admin.local>
2018-12-27 16:33:31 +0300

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Admin on 27/12/2018.
+//  Copyright © 2018 Andrey Kruglov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
